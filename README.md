4a57107443987bf350b64d5fe9a2f56978889b18
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-07 16:21:43 -0400

    Project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+### Calculator Captone Project
+
+This is the capstone project for The Odin Project fundamentals course.
+
+In this project I will create a calculator UI, style it with some simple CSS, and attach functions so that it runs correctly.
+
+In the final stages of the project I anticipate a lot of debugging while I handle various user scenarios.
---
+++ calculator.js
@@ -0,0 +1,75 @@
+// starting out with math operations from a prior project
+const add = function(num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return NaN;
+  }
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return NaN;
+  }
+  return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return NaN;
+  }
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return NaN;
+  }
+  return num1 - num2;
+};
+
+const sum = function(arr) {
+  let sum = 0;
+  if(arr.length === 0) {
+    return 0;
+  }
+  // check if the array contains only numbers
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number') {
+      return NaN;
+    }
+    sum += arr[i];
+  }
+  return sum;
+};
+
+const multiply = function(args) {
+  multiple = 1;
+  // check if the arguments are numbers
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] !== 'number') {
+      return NaN;
+    }
+    multiple *= args[i];
+  }
+  return multiple;
+};
+
+const power = function(num1, num2) {
+  if (num1 === undefined || num2 === undefined) {
+    return NaN;
+  }
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return NaN;
+  }
+  return num1 ** num2;	
+};
+
+const factorial = function(num1) {
+  if (num1 === 0) {
+    return 1;
+  } else {
+    return num1 * factorial(num1 - 1);
+  }
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
