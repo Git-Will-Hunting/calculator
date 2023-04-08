b9946a394be65ea804a38a5e29061736e0c954e8
git-will-hunting <58862067+Git-Will-Hunting@users.noreply.github.com>
2023-04-08 02:14:59 -0400

    Calculate using an array of 2 numbers

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// gather UI elements
+const displayTop = document.querySelector('.calcDisplayTop');
+const displayBottom = document.querySelector('.calcDisplayBottom');
+const buttons = document.querySelectorAll('.calcButton');
+
 // set global variables for math functions
-let num1 = 0;
-let num2 = 0;
+let num = [0,0];
+
 let result = 0;
 let operator = '';
 
@@ -72,7 +77,10 @@ function divide(num1, num2){
 }
 
 // operator function to run the calculator
-function operate(operator, num1, num2){
+function operate(operator){
+
+    const num1 = num[num.length -2]
+    const num2 = num[num.length -1]
     if (operator === '+') {
         return add(num1, num2);
     }
@@ -92,3 +100,98 @@ function operate(operator, num1, num2){
         return factorial(num1);
     }
 }
+
+function updateNum() {
+    num.push(parseFloat(displayBottom.textContent));
+    num.reverse();
+    num.pop();
+    num.reverse();
+    displayBottom.textContent = '';
+}
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        // if the button is a number, add it to the display
+        if (button.classList.contains('number')) {
+            if (displayBottom.textContent === '0') {
+                displayBottom.textContent = '';
+            }
+            displayBottom.textContent += button.textContent;
+        }
+        // if the button is an operator, store the number and operator
+        if (button.classList.contains('calcOp')) {
+            updateNum();
+            operator = button.name;
+            result = operate(operator);
+            displayTop.textContent = result;
+        }
+        // if the button is the equal sign, store the second number and run the operator function
+        if (button.classList.contains('equals')) {
+            updateNum();
+            result = operate(operator);
+            displayTop.textContent = result;
+        }
+        // if the button is the clear sign, clear the display
+        if (button.classList.contains('clear')) {
+            num = [0,0];
+            displayBottom.textContent = '';
+        }
+        // if the button is the negative sign, add a negative sign to the display
+        if (button.classList.contains('negative')) {
+            displayBottom.textContent = '-' + displayBottom.textContent;
+        }
+        // if the button is the decimal sign, add a decimal to the display
+        if (button.classList.contains('decimal')) {
+            displayBottom.textContent += '.';
+        }
+        // if the button is the percent sign, divide the number by 100
+        if (button.classList.contains('percent')) {
+            displayTop.textContent = parseFloat(displayBottom.textContent) / 100;
+        }
+    });
+});
+
+addEventListener('keydown', (e) => {
+    // if the key is a number, add it to the display
+    if (e.key >= 0 && e.key <= 9) {
+        if (displayBottom.textContent === '0') {
+            displayBottom.textContent = '';
+        }
+        displayBottom.textContent += e.key;
+    }
+    // if the key is x, change it to *
+    if (e.key === 'x') {
+        e.key = '*';
+    }
+    // if the key is an operator, store the number and operator
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        updateNum();
+        operator = e.key;
+        result = operate(operator);
+        displayTop.textContent = result;
+    }
+    // if the key is the equal sign, store the second number and run the operator function
+    if (e.key === 'Enter') {
+        updateNum();
+        result = operate(operator);
+        displayTop.textContent = result;
+    }
+    // if the key is the clear sign, clear the display
+    if (e.key === 'Backspace' || e.key === 'c' || e.key === 'Delete') {
+        num = [0,0];
+        displayBottom.textContent = '';
+    }
+    // if the key is the negative sign, add a negative sign to the display
+    if (e.key === 'n') {
+        displayBottom.textContent = '-' + displayBottom.textContent;
+    }
+    // if the key is the decimal sign, add a decimal to the display
+    if (e.key === '.') {
+        displayBottom.textContent += '.';
+    }
+    // if the key is the percent sign, divide the number by 100
+    if (e.key === '%') {
+        displayTop.textContent = parseFloat(displayBottom.textContent) / 100;
+    }
+});
